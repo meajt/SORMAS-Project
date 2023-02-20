d20dd11fe810c8b6e98734c4d8e03fa4126febc9
aj sharma <meaj370@gmail.com>
2023-02-20 11:49:59 +0545

    added mobile no field in person case edit form

--- sormas-ui/src/main/java/de/symeda/sormas/ui/person/PersonEditForm.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/person/PersonEditForm.java
@@ -136,7 +136,7 @@ public class PersonEditForm extends AbstractEditForm<PersonDto> {
                     fluidRowLocs(PersonDto.PASSPORT_NUMBER, PersonDto.NATIONAL_HEALTH_ID) +
 					fluidRowLocs(PersonDto.EXTERNAL_ID, PersonDto.EXTERNAL_TOKEN) +
 					fluidRowLocs(PersonDto.INTERNAL_TOKEN, EXTERNAL_TOKEN_WARNING_LOC) +
-
+					fluidRowLocs(PersonDto.MOBILE_NO, "") +
 					fluidRowLocs(PersonDto.HAS_COVID_APP, PersonDto.COVID_CODE_DELIVERED) +
 
                     loc(OCCUPATION_HEADER) +
@@ -375,6 +375,7 @@ public class PersonEditForm extends AbstractEditForm<PersonDto> {
 		externalTokenWarningLabel.addStyleNames(VSPACE_3, LABEL_WHITE_SPACE_NORMAL);
 		getContent().addComponent(externalTokenWarningLabel, EXTERNAL_TOKEN_WARNING_LOC);
 		addField(PersonDto.INTERNAL_TOKEN);
+		addField(PersonDto.MOBILE_NO, TextField.class);
 
 		addField(PersonDto.HAS_COVID_APP).addStyleName(CssStyles.FORCE_CAPTION_CHECKBOX);
 		addField(PersonDto.COVID_CODE_DELIVERED).addStyleName(CssStyles.FORCE_CAPTION_CHECKBOX);
