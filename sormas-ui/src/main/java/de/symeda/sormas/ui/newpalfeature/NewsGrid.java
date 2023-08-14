f82582b123c6c0822eb4f874a4590728f345bf06
aj sharma <meaj370@gmail.com>
2023-08-14 16:34:44 +0545

    feat: remove column link form news grid

--- sormas-ui/src/main/java/de/symeda/sormas/ui/newpalfeature/NewsGrid.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/newpalfeature/NewsGrid.java
@@ -18,7 +18,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class NewsGrid extends FilteredGrid<NewsDto, NewsCriteria> {
-    private final static String OPEN_LINK_ACTION = "openLinkAction";
     private final static String CREATE_EVENT_ACTION = "createEventAction";
     final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
@@ -27,41 +26,39 @@ public class NewsGrid extends FilteredGrid<NewsDto, NewsCriteria> {
         setSizeFull();
         setLazyDataProvider();
         setCriteria(criteria);
-        Column<NewsDto, String> openLickColum = addColumn(entry -> VaadinIcons.EXTERNAL_LINK.getHtml(), new HtmlRenderer());
-        openLickColum.setId(OPEN_LINK_ACTION);
-        openLickColum.setCaption("Open Link");
-        openLickColum.setWidth(100);
-
-        addItemClickListener(new ShowDetailsListener<>(OPEN_LINK_ACTION, e -> getUI().getPage().open(e.getNewsLink(), "_blank")));
-
-
         Column<NewsDto, String> createEvent = addColumn(entry -> VaadinIcons.PLUS_CIRCLE.getHtml(), new HtmlRenderer());
         createEvent.setId(CREATE_EVENT_ACTION);
         createEvent.setCaption("Create Event");
         createEvent.setWidth(100);
-        addItemClickListener(new ShowDetailsListener<>(CREATE_EVENT_ACTION, news -> {
-            ControllerProvider.getEventController().create(event -> {
-                event.setEventTitle(news.getTitle());
-                event.setEventDesc(news.getSummary());
-                event.setSrcType(EventSourceType.MEDIA_NEWS);
-                event.setSrcMediaWebsite(news.getNewsLink());
-                event.setSrcMediaName(news.getNewsSource());
-                event.setRiskLevel(actionPriorityFromCaption(news.getEpidemiologicalRiskLevel()));
+        addItemClickListener(new ShowDetailsListener<>(CREATE_EVENT_ACTION, this::createEventFromNews));
 
-                try {
-                    event.setStartDate(dateFormat.parse(news.getDate()));
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-
-            });
-        }));
-        setColumns(NewsDto.TITLE, NewsDto.SUMMARY,  NewsDto.PROVINCE,  NewsDto.DISTRICT,  NewsDto.PALIKA, NewsDto.DATE, NewsDto.RISK_LEVEL, OPEN_LINK_ACTION, CREATE_EVENT_ACTION);
+        setColumns(NewsDto.TITLE, NewsDto.SUMMARY,  NewsDto.PROVINCE,  NewsDto.DISTRICT,  NewsDto.PALIKA, NewsDto.DATE, NewsDto.RISK_LEVEL, CREATE_EVENT_ACTION);
         getColumn(NewsDto.SUMMARY).setWidth(600);
         getColumn(NewsDto.PROVINCE).setWidth(80);
         getColumn(NewsDto.DISTRICT).setWidth(80);
         getColumn(NewsDto.RISK_LEVEL).setWidth(80);
+        addItemClickListener(new ShowDetailsListener<>(NewsDto.TITLE, e -> getUI().getPage().open(e.getNewsLink(), "_blank")));
+
     }
+
+    private void createEventFromNews(NewsDto news) {
+        ControllerProvider.getEventController().create(event -> {
+            event.setEventTitle(news.getTitle());
+            event.setEventDesc(news.getSummary());
+            event.setSrcType(EventSourceType.MEDIA_NEWS);
+            event.setSrcMediaWebsite(news.getNewsLink());
+            event.setSrcMediaName(news.getNewsSource());
+            event.setRiskLevel(actionPriorityFromCaption(news.getEpidemiologicalRiskLevel()));
+
+            try {
+                event.setStartDate(dateFormat.parse(news.getDate()));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+        });
+    }
+
     private  RiskLevel actionPriorityFromCaption(String priorityCaption) {
         if (I18nProperties.getEnumCaption(RiskLevel.HIGH).equals(priorityCaption))
             return RiskLevel.HIGH;
