94a79daccd07397cfc14bdc59dab501300b93f2a
Aj sharma <meaj370@gmail.com>
2023-09-30 23:44:30 +0545

    uncommented cron code

--- sormas-backend/src/main/java/de/symeda/sormas/backend/common/CronService.java
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/common/CronService.java
@@ -91,187 +91,187 @@ public class CronService {
 	@EJB
 	private CoreEntityDeletionService coreEntityDeletionService;
 
-//	@Schedule(hour = "*", minute = "*/" + TASK_UPDATE_INTERVAL, second = "0", persistent = false)
-//	public void sendNewAndDueTaskMessages() {
-//		taskFacade.sendNewAndDueTaskMessages();
-//	}
-//
-//	@Schedule(hour = "*", minute = "*/2", second = "0", persistent = false)
-//	public void calculateCaseCompletion() {
-//		long timeStart = DateHelper.startTime();
-//		int casesUpdated = caseFacade.updateCompleteness();
-//		logger.debug("calculateCaseCompletion finished. {} cases, {} s", casesUpdated, DateHelper.durationSeconds(timeStart));
-//	}
-//
-//	@Schedule(hour = "1", minute = "0", second = "0", persistent = false)
-//	public void deleteAllExpiredFeatureConfigurations() {
-//
-//		// Remove all feature configurations whose end dates have been reached
-//		featureConfigurationFacade.deleteAllExpiredFeatureConfigurations(new Date());
-//		logger.info("Deleted expired feature configurations");
-//	}
-//
-//	@Schedule(hour = "1", minute = "5", second = "0", persistent = false)
-//	public void generateAutomaticTasks() {
-//
-//		if (featureConfigurationFacade.isTaskGenerationFeatureEnabled(TaskType.CONTACT_FOLLOW_UP)) {
-//			contactFacade.generateContactFollowUpTasks();
-//		}
-//		if (featureConfigurationFacade.isTaskGenerationFeatureEnabled(TaskType.WEEKLY_REPORT_GENERATION)) {
-//			weeklyReportFacade.generateSubmitWeeklyReportTasks();
-//		}
-//	}
-//
-//	@Schedule(hour = "1", minute = "10", second = "0", persistent = false)
-//	public void cleanUpTemporaryFiles() {
-//
-//		Date now = new Date();
-//		File exportFolder = new File(configFacade.getTempFilesPath());
-//
-//		int numberOfDeletedFiles = 0;
-//		final File[] files = exportFolder.listFiles();
-//		if (files == null) {
-//			logger.warn("No files found in export folder {}", exportFolder.getAbsolutePath());
-//			return;
-//		}
-//
-//		for (final File fileEntry : files) {
-//			// Skip the file if it's a directory or not a temporary sormas file
-//			if (!fileEntry.isFile() || (!fileEntry.getName().startsWith(ImportExportUtils.TEMP_FILE_PREFIX))) {
-//				continue;
-//			}
-//
-//			try {
-//				BasicFileAttributes fileAttributes = Files.readAttributes(fileEntry.toPath(), BasicFileAttributes.class);
-//				if (now.getTime() - fileAttributes.creationTime().toMillis() >= 1000 * 60 * 120) {
-//					Files.delete(fileEntry.toPath());
-//					numberOfDeletedFiles++;
-//				}
-//			} catch (IOException e) {
-//				logger.info("Error deleting a file in CronService. The file in question was " + fileEntry.getAbsolutePath(), e);
-//			}
-//		}
-//
-//		logger.info("Deleted {} export files", numberOfDeletedFiles);
-//	}
-//
-//	@Schedule(hour = "1", minute = "15", second = "0", persistent = false)
-//	public void archiveCases() {
-//
-//		final int daysAfterCaseGetsArchived = featureConfigurationFacade
-//			.getProperty(FeatureType.AUTOMATIC_ARCHIVING, CoreEntityType.CASE, FeatureTypeProperty.THRESHOLD_IN_DAYS, Integer.class);
-//		final int daysAfterContactsGetsArchived = featureConfigurationFacade
-//			.getProperty(FeatureType.AUTOMATIC_ARCHIVING, CoreEntityType.CONTACT, FeatureTypeProperty.THRESHOLD_IN_DAYS, Integer.class);
-//		if (daysAfterCaseGetsArchived < daysAfterContactsGetsArchived) {
-//			logger.warn(
-//				"{} for {} [{}] should be <= the one for {} [{}]",
-//				FeatureTypeProperty.THRESHOLD_IN_DAYS,
-//				CoreEntityType.CONTACT,
-//				CoreEntityType.CASE,
-//				daysAfterContactsGetsArchived,
-//				daysAfterCaseGetsArchived);
-//		}
-//		if (daysAfterCaseGetsArchived >= 1) {
-//			caseFacade.archiveAllArchivableCases(daysAfterCaseGetsArchived);
-//		}
-//	}
-//
-//	@Schedule(hour = "1", minute = "20", second = "0", persistent = false)
-//	public void archiveEvents() {
-//
-//		final int daysAfterEventsGetsArchived = featureConfigurationFacade
-//			.getProperty(FeatureType.AUTOMATIC_ARCHIVING, CoreEntityType.EVENT, FeatureTypeProperty.THRESHOLD_IN_DAYS, Integer.class);
-//		final int daysAfterEventParticipantsGetsArchived = featureConfigurationFacade
-//			.getProperty(FeatureType.AUTOMATIC_ARCHIVING, CoreEntityType.EVENT_PARTICIPANT, FeatureTypeProperty.THRESHOLD_IN_DAYS, Integer.class);
-//		if (daysAfterEventsGetsArchived < daysAfterEventParticipantsGetsArchived) {
-//			logger.warn(
-//				"{} for {} [{}] should be <= the one for {} [{}]",
-//				FeatureTypeProperty.THRESHOLD_IN_DAYS,
-//				CoreEntityType.EVENT_PARTICIPANT,
-//				CoreEntityType.EVENT,
-//				daysAfterEventParticipantsGetsArchived,
-//				daysAfterEventsGetsArchived);
-//		}
-//		if (daysAfterEventsGetsArchived >= 1) {
-//			eventFacade.archiveAllArchivableEvents(daysAfterEventsGetsArchived);
-//		}
-//	}
-//
-//	@Schedule(hour = "1", minute = "25", second = "0", persistent = false)
-//	public void cleanupDeletedDocuments() {
-//		documentFacade.cleanupDeletedDocuments();
-//	}
-//
-//	@Schedule(hour = "1", minute = "30", second = "0", persistent = false)
-//	public void deleteSystemEvents() {
-//		int daysAfterSystemEventGetsDeleted = configFacade.getDaysAfterSystemEventGetsDeleted();
-//		if (daysAfterSystemEventGetsDeleted >= 1) {
-//			systemEventFacade.deleteAllDeletableSystemEvents(daysAfterSystemEventGetsDeleted);
-//		}
-//	}
-//
-//	@Schedule(hour = "1", minute = "35", second = "0", persistent = false)
-//	public void fetchExternalMessages() {
-//		if (featureConfigurationFacade.isFeatureEnabled(FeatureType.EXTERNAL_MESSAGES)) {
-//			externalMessageFacade.fetchAndSaveExternalMessages(null);
-//		}
-//	}
-//
-//	@Schedule(hour = "1", minute = "40", second = "0", persistent = false)
-//	public void updateImmunizationStatuses() {
-//		if (featureConfigurationFacade.isFeatureEnabled(FeatureType.IMMUNIZATION_STATUS_AUTOMATION)) {
-//			immunizationFacade.updateImmunizationStatuses();
-//		}
-//	}
-//
-//	@Schedule(hour = "1", minute = "50", persistent = false)
-//	public void syncInfraWithCentral() {
-//		centralInfraSyncFacade.syncAll();
-//	}
-//
-//	@Schedule(hour = "1", minute = "55", persistent = false)
-//	public void deleteExpiredEntities() {
-//		coreEntityDeletionService.executeAutomaticDeletion();
-//	}
-//
-//	@Schedule(hour = "2", minute = "15", persistent = false)
-//	public void archiveContacts() {
-//		final int daysAfterContactsGetsArchived = featureConfigurationFacade
-//			.getProperty(FeatureType.AUTOMATIC_ARCHIVING, CoreEntityType.CONTACT, FeatureTypeProperty.THRESHOLD_IN_DAYS, Integer.class);
-//
-//		if (daysAfterContactsGetsArchived >= 1) {
-//			contactFacade.archiveAllArchivableContacts(daysAfterContactsGetsArchived);
-//		}
-//	}
-//
-//	@Schedule(hour = "2", minute = "20", persistent = false)
-//	public void archiveEventParticipants() {
-//		final int daysAfterEventParticipantGetsArchived = featureConfigurationFacade
-//			.getProperty(FeatureType.AUTOMATIC_ARCHIVING, CoreEntityType.EVENT_PARTICIPANT, FeatureTypeProperty.THRESHOLD_IN_DAYS, Integer.class);
-//
-//		if (daysAfterEventParticipantGetsArchived >= 1) {
-//			eventParticipantFacade.archiveAllArchivableEventParticipants(daysAfterEventParticipantGetsArchived);
-//		}
-//	}
-//
-//	@Schedule(hour = "2", minute = "25", persistent = false)
-//	public void archiveImmunizations() {
-//		final int daysAfterImmunizationsGetsArchived = featureConfigurationFacade
-//			.getProperty(FeatureType.AUTOMATIC_ARCHIVING, CoreEntityType.IMMUNIZATION, FeatureTypeProperty.THRESHOLD_IN_DAYS, Integer.class);
-//
-//		if (daysAfterImmunizationsGetsArchived >= 1) {
-//			immunizationFacade.archiveAllArchivableImmunizations(daysAfterImmunizationsGetsArchived);
-//		}
-//	}
-//
-//	@Schedule(hour = "2", minute = "30", persistent = false)
-//	public void archiveTravelEntry() {
-//		final int daysAfterTravelEntryGetsArchived = featureConfigurationFacade
-//			.getProperty(FeatureType.AUTOMATIC_ARCHIVING, CoreEntityType.TRAVEL_ENTRY, FeatureTypeProperty.THRESHOLD_IN_DAYS, Integer.class);
-//
-//		if (daysAfterTravelEntryGetsArchived >= 1) {
-//			travelEntryFacade.archiveAllArchivableTravelEntries(daysAfterTravelEntryGetsArchived);
-//		}
-//	}
+	@Schedule(hour = "*", minute = "*/" + TASK_UPDATE_INTERVAL, second = "0", persistent = false)
+	public void sendNewAndDueTaskMessages() {
+		taskFacade.sendNewAndDueTaskMessages();
+	}
+
+	@Schedule(hour = "*", minute = "*/2", second = "0", persistent = false)
+	public void calculateCaseCompletion() {
+		long timeStart = DateHelper.startTime();
+		int casesUpdated = caseFacade.updateCompleteness();
+		logger.debug("calculateCaseCompletion finished. {} cases, {} s", casesUpdated, DateHelper.durationSeconds(timeStart));
+	}
+
+	@Schedule(hour = "1", minute = "0", second = "0", persistent = false)
+	public void deleteAllExpiredFeatureConfigurations() {
+
+		// Remove all feature configurations whose end dates have been reached
+		featureConfigurationFacade.deleteAllExpiredFeatureConfigurations(new Date());
+		logger.info("Deleted expired feature configurations");
+	}
+
+	@Schedule(hour = "1", minute = "5", second = "0", persistent = false)
+	public void generateAutomaticTasks() {
+
+		if (featureConfigurationFacade.isTaskGenerationFeatureEnabled(TaskType.CONTACT_FOLLOW_UP)) {
+			contactFacade.generateContactFollowUpTasks();
+		}
+		if (featureConfigurationFacade.isTaskGenerationFeatureEnabled(TaskType.WEEKLY_REPORT_GENERATION)) {
+			weeklyReportFacade.generateSubmitWeeklyReportTasks();
+		}
+	}
+
+	@Schedule(hour = "1", minute = "10", second = "0", persistent = false)
+	public void cleanUpTemporaryFiles() {
+
+		Date now = new Date();
+		File exportFolder = new File(configFacade.getTempFilesPath());
+
+		int numberOfDeletedFiles = 0;
+		final File[] files = exportFolder.listFiles();
+		if (files == null) {
+			logger.warn("No files found in export folder {}", exportFolder.getAbsolutePath());
+			return;
+		}
+
+		for (final File fileEntry : files) {
+			// Skip the file if it's a directory or not a temporary sormas file
+			if (!fileEntry.isFile() || (!fileEntry.getName().startsWith(ImportExportUtils.TEMP_FILE_PREFIX))) {
+				continue;
+			}
+
+			try {
+				BasicFileAttributes fileAttributes = Files.readAttributes(fileEntry.toPath(), BasicFileAttributes.class);
+				if (now.getTime() - fileAttributes.creationTime().toMillis() >= 1000 * 60 * 120) {
+					Files.delete(fileEntry.toPath());
+					numberOfDeletedFiles++;
+				}
+			} catch (IOException e) {
+				logger.info("Error deleting a file in CronService. The file in question was " + fileEntry.getAbsolutePath(), e);
+			}
+		}
+
+		logger.info("Deleted {} export files", numberOfDeletedFiles);
+	}
+
+	@Schedule(hour = "1", minute = "15", second = "0", persistent = false)
+	public void archiveCases() {
+
+		final int daysAfterCaseGetsArchived = featureConfigurationFacade
+			.getProperty(FeatureType.AUTOMATIC_ARCHIVING, CoreEntityType.CASE, FeatureTypeProperty.THRESHOLD_IN_DAYS, Integer.class);
+		final int daysAfterContactsGetsArchived = featureConfigurationFacade
+			.getProperty(FeatureType.AUTOMATIC_ARCHIVING, CoreEntityType.CONTACT, FeatureTypeProperty.THRESHOLD_IN_DAYS, Integer.class);
+		if (daysAfterCaseGetsArchived < daysAfterContactsGetsArchived) {
+			logger.warn(
+				"{} for {} [{}] should be <= the one for {} [{}]",
+				FeatureTypeProperty.THRESHOLD_IN_DAYS,
+				CoreEntityType.CONTACT,
+				CoreEntityType.CASE,
+				daysAfterContactsGetsArchived,
+				daysAfterCaseGetsArchived);
+		}
+		if (daysAfterCaseGetsArchived >= 1) {
+			caseFacade.archiveAllArchivableCases(daysAfterCaseGetsArchived);
+		}
+	}
+
+	@Schedule(hour = "1", minute = "20", second = "0", persistent = false)
+	public void archiveEvents() {
+
+		final int daysAfterEventsGetsArchived = featureConfigurationFacade
+			.getProperty(FeatureType.AUTOMATIC_ARCHIVING, CoreEntityType.EVENT, FeatureTypeProperty.THRESHOLD_IN_DAYS, Integer.class);
+		final int daysAfterEventParticipantsGetsArchived = featureConfigurationFacade
+			.getProperty(FeatureType.AUTOMATIC_ARCHIVING, CoreEntityType.EVENT_PARTICIPANT, FeatureTypeProperty.THRESHOLD_IN_DAYS, Integer.class);
+		if (daysAfterEventsGetsArchived < daysAfterEventParticipantsGetsArchived) {
+			logger.warn(
+				"{} for {} [{}] should be <= the one for {} [{}]",
+				FeatureTypeProperty.THRESHOLD_IN_DAYS,
+				CoreEntityType.EVENT_PARTICIPANT,
+				CoreEntityType.EVENT,
+				daysAfterEventParticipantsGetsArchived,
+				daysAfterEventsGetsArchived);
+		}
+		if (daysAfterEventsGetsArchived >= 1) {
+			eventFacade.archiveAllArchivableEvents(daysAfterEventsGetsArchived);
+		}
+	}
+
+	@Schedule(hour = "1", minute = "25", second = "0", persistent = false)
+	public void cleanupDeletedDocuments() {
+		documentFacade.cleanupDeletedDocuments();
+	}
+
+	@Schedule(hour = "1", minute = "30", second = "0", persistent = false)
+	public void deleteSystemEvents() {
+		int daysAfterSystemEventGetsDeleted = configFacade.getDaysAfterSystemEventGetsDeleted();
+		if (daysAfterSystemEventGetsDeleted >= 1) {
+			systemEventFacade.deleteAllDeletableSystemEvents(daysAfterSystemEventGetsDeleted);
+		}
+	}
+
+	@Schedule(hour = "1", minute = "35", second = "0", persistent = false)
+	public void fetchExternalMessages() {
+		if (featureConfigurationFacade.isFeatureEnabled(FeatureType.EXTERNAL_MESSAGES)) {
+			externalMessageFacade.fetchAndSaveExternalMessages(null);
+		}
+	}
+
+	@Schedule(hour = "1", minute = "40", second = "0", persistent = false)
+	public void updateImmunizationStatuses() {
+		if (featureConfigurationFacade.isFeatureEnabled(FeatureType.IMMUNIZATION_STATUS_AUTOMATION)) {
+			immunizationFacade.updateImmunizationStatuses();
+		}
+	}
+
+	@Schedule(hour = "1", minute = "50", persistent = false)
+	public void syncInfraWithCentral() {
+		centralInfraSyncFacade.syncAll();
+	}
+
+	@Schedule(hour = "1", minute = "55", persistent = false)
+	public void deleteExpiredEntities() {
+		coreEntityDeletionService.executeAutomaticDeletion();
+	}
+
+	@Schedule(hour = "2", minute = "15", persistent = false)
+	public void archiveContacts() {
+		final int daysAfterContactsGetsArchived = featureConfigurationFacade
+			.getProperty(FeatureType.AUTOMATIC_ARCHIVING, CoreEntityType.CONTACT, FeatureTypeProperty.THRESHOLD_IN_DAYS, Integer.class);
+
+		if (daysAfterContactsGetsArchived >= 1) {
+			contactFacade.archiveAllArchivableContacts(daysAfterContactsGetsArchived);
+		}
+	}
+
+	@Schedule(hour = "2", minute = "20", persistent = false)
+	public void archiveEventParticipants() {
+		final int daysAfterEventParticipantGetsArchived = featureConfigurationFacade
+			.getProperty(FeatureType.AUTOMATIC_ARCHIVING, CoreEntityType.EVENT_PARTICIPANT, FeatureTypeProperty.THRESHOLD_IN_DAYS, Integer.class);
+
+		if (daysAfterEventParticipantGetsArchived >= 1) {
+			eventParticipantFacade.archiveAllArchivableEventParticipants(daysAfterEventParticipantGetsArchived);
+		}
+	}
+
+	@Schedule(hour = "2", minute = "25", persistent = false)
+	public void archiveImmunizations() {
+		final int daysAfterImmunizationsGetsArchived = featureConfigurationFacade
+			.getProperty(FeatureType.AUTOMATIC_ARCHIVING, CoreEntityType.IMMUNIZATION, FeatureTypeProperty.THRESHOLD_IN_DAYS, Integer.class);
+
+		if (daysAfterImmunizationsGetsArchived >= 1) {
+			immunizationFacade.archiveAllArchivableImmunizations(daysAfterImmunizationsGetsArchived);
+		}
+	}
+
+	@Schedule(hour = "2", minute = "30", persistent = false)
+	public void archiveTravelEntry() {
+		final int daysAfterTravelEntryGetsArchived = featureConfigurationFacade
+			.getProperty(FeatureType.AUTOMATIC_ARCHIVING, CoreEntityType.TRAVEL_ENTRY, FeatureTypeProperty.THRESHOLD_IN_DAYS, Integer.class);
+
+		if (daysAfterTravelEntryGetsArchived >= 1) {
+			travelEntryFacade.archiveAllArchivableTravelEntries(daysAfterTravelEntryGetsArchived);
+		}
+	}
 
 }
